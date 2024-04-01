e3e4b1d805d0ea608bf6a545acd3f0050cf66a6d
lihang <1019825699@qq.com>
2024-04-02 00:19:39 +0800

    “增加前端的头文件”

---
+++ include/modules/frontend/frontend.hh
@@ -0,0 +1,36 @@
+#pragma once
+#include "modules/module_base.hh"
+
+#include <deque>
+#include "type/imu.hh"
+#include "type/pointcloud.hh"
+#include "type/pose.hh"
+#include "utils/lidar_utils.hh"
+
+namespace IESKF_SLAM {
+class Frontend : public ModuleBase {
+   public:
+    using Ptr = std::shared_ptr<Frontend>;
+
+   public:
+    Frontend();
+    ~Frontend();
+
+    // 传入数据给前端
+    void AddIMU(const IMU& imu_data);
+    void AddCloud(const PointCloud& cloud);
+    void AddPose(const Pose& pose);
+
+    bool track();
+
+    const PCLPointCloud& getCurrentCloud() const {
+        return current_pointcloud_;
+    }
+
+   private:
+    std::deque<IMU> imu_queue_;
+    std::deque<PointCloud> clouds_queue_;
+    std::deque<Pose> pose_queue_;
+    PCLPointCloud current_pointcloud_;
+};
+}  // namespace IESKF_SLAM
---
+++ include/modules/module_base.hh
@@ -0,0 +1,49 @@
+/*
+ * @Author: lihang 1019825699@qq.com
+ * @Date: 2024-04-01 00:30:12
+ * @LastEditors: lihang 1019825699@qq.com
+ * @LastEditTime: 2024-04-02 00:00:00
+ * @FilePath: /lio_ws/src/ieskf_slam/include/modules/module_base.hh
+ * @Description: 几乎所有的模块都需要通过配置文件来修改参数，所以定义一个基类
+ *
+ * Copyright (c) 2024 by ${git_name_email}, All Rights Reserved.
+ */
+#pragma once
+#include <yaml-cpp/yaml.h>
+#include <iostream>
+#include <string>
+
+namespace IESKF_SLAM {
+class ModuleBase {
+   private:
+    YAML::Node config_node_;
+    std::string module_name_;
+
+   protected:
+    ModuleBase(const std::string& config_path, const std::string& prefix, const std::string& module_name = "default") {
+        module_name_ = module_name;
+        if (config_path != "") {
+            try {
+                // LoadFile 返回一个node
+                config_node_ = YAML::LoadFile(config_path);
+            } catch (YAML::Exception& e) {
+                std::cout << e.what() << std::endl;
+            }
+            if (prefix != "" && config_node_[prefix]) {
+                config_node_ = config_node_[prefix];
+            }
+        }
+    }
+    /**
+     * @brief 读取参数
+     */
+    template <typename T>
+    void readParam(const std::string& key, T& value, T default_value) {
+        if (config_node_[key]) {
+            value = config_node_[key].as<T>();
+        } else {
+            value = default_value;
+        }
+    }
+};
+}  // namespace IESKF_SLAM
