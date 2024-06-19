ceee814351957fbb41fcedb76945755cc464dec2
lihang <1019825699@qq.com>
2024-06-19 02:21:48 +0000

    "add voxelmap"

---
+++ src/voxel_map/voxel_map.cc
@@ -0,0 +1,316 @@
+#include "voxel_map/voxel_map.hh"
+
+namespace lio {
+size_t HashUtil::operator()(const Eigen::Vector3d& point) {
+    Eigen::Vector3d loc_xyz;
+    for (size_t i = 0; i < 3; ++i) {
+        loc_xyz(i) = point[i] * resolution_inv_;
+        if (loc_xyz[i] < 0) {
+            loc_xyz[i] -= 1.0;
+        }
+    }
+    size_t x = static_cast<size_t>(loc_xyz[0]);
+    size_t y = static_cast<size_t>(loc_xyz[1]);
+    size_t z = static_cast<size_t>(loc_xyz[2]);
+    return (((z * hash_p_) % max_n_ + y) * hash_p_) % max_n_ + x;
+}
+
+void Grid::setMinMax(size_t min, size_t max) {
+    min_num = min;
+    max_num = max;
+}
+
+void Grid::addPoint(const Eigen::Vector3d& point, bool insert) {
+    point_num++;
+    is_update = false;
+    points_sum += point;
+    centroid = points_sum / static_cast<double>(point_num);
+    conv_sum += (point * point.transpose());
+    if (points.size() >= 2 * max_num || !insert) return;
+    points.push_back(point);
+}
+
+// 更新协方差
+void Grid::updateConv() {
+    // 已经更新过了
+    if (is_update) {
+        return;
+    }
+    is_update = true;
+    // 体素中的点过小
+    if (point_num < min_num) {
+        is_valid = false;
+        return;
+    }
+    // 更新协方差
+    conv = (conv_sum - point_num * centroid.transpose()) / (static_cast<double>(point_num) - 1);
+    is_valid = true;
+    Eigen::JacobiSVD<Eigen::Matrix3d> svd(conv, Eigen::ComputeFullU | Eigen::ComputeFullV);
+    Eigen::Vector3d val(1, 1, 1e-3);
+    conv = svd.matrixU() * val.asDiagonal() * svd.matrixV().transpose();
+    // conv_inv = conv.inverse();
+}
+
+void VoxelMap::initializeNearby() {
+    nearby_.clear();
+    switch (mode_) {
+        case NEARBY1:
+            /* code */
+            nearby_.push_back(Eigen::Vector3d(0, 0, 0));
+            break;
+        case NEARBY7:
+            nearby_.push_back(Eigen::Vector3d(0.0, 0.0, 0.0));
+            nearby_.push_back(Eigen::Vector3d(resolution_, 0.0, 0.0));
+            nearby_.push_back(Eigen::Vector3d(-resolution_, 0.0, 0.0));
+            nearby_.push_back(Eigen::Vector3d(0.0, resolution_, 0.0));
+            nearby_.push_back(Eigen::Vector3d(0.0, -resolution_, 0.0));
+            nearby_.push_back(Eigen::Vector3d(0.0, 0.0, resolution_));
+            nearby_.push_back(Eigen::Vector3d(0.0, 0.0, -resolution_));
+            break;
+        case NEARBY19:
+            nearby_.push_back(Eigen::Vector3d(0.0, 0.0, 0.0));
+            nearby_.push_back(Eigen::Vector3d(resolution_, 0.0, 0.0));
+            nearby_.push_back(Eigen::Vector3d(-resolution_, 0.0, 0.0));
+            nearby_.push_back(Eigen::Vector3d(0.0, resolution_, 0.0));
+            nearby_.push_back(Eigen::Vector3d(0.0, -resolution_, 0.0));
+            nearby_.push_back(Eigen::Vector3d(0.0, 0.0, resolution_));
+            nearby_.push_back(Eigen::Vector3d(0.0, 0.0, -resolution_));
+            nearby_.push_back(Eigen::Vector3d(resolution_, resolution_, 0.0));
+            nearby_.push_back(Eigen::Vector3d(resolution_, -resolution_, 0.0));
+            nearby_.push_back(Eigen::Vector3d(resolution_, 0.0, resolution_));
+            nearby_.push_back(Eigen::Vector3d(resolution_, 0.0, -resolution_));
+            nearby_.push_back(Eigen::Vector3d(0.0, resolution_, resolution_));
+            nearby_.push_back(Eigen::Vector3d(0.0, resolution_, -resolution_));
+            nearby_.push_back(Eigen::Vector3d(-resolution_, resolution_, 0.0));
+            nearby_.push_back(Eigen::Vector3d(-resolution_, -resolution_, 0.0));
+            nearby_.push_back(Eigen::Vector3d(-resolution_, 0.0, resolution_));
+            nearby_.push_back(Eigen::Vector3d(-resolution_, 0.0, -resolution_));
+            nearby_.push_back(Eigen::Vector3d(0.0, -resolution_, resolution_));
+            nearby_.push_back(Eigen::Vector3d(0.0, -resolution_, -resolution_));
+            break;
+        default:
+            nearby_.push_back(Eigen::Vector3d(0.0, 0.0, 0.0));
+            nearby_.push_back(Eigen::Vector3d(resolution_, 0.0, 0.0));
+            nearby_.push_back(Eigen::Vector3d(-resolution_, 0.0, 0.0));
+            nearby_.push_back(Eigen::Vector3d(0.0, resolution_, 0.0));
+            nearby_.push_back(Eigen::Vector3d(0.0, -resolution_, 0.0));
+            nearby_.push_back(Eigen::Vector3d(0.0, 0.0, resolution_));
+            nearby_.push_back(Eigen::Vector3d(0.0, 0.0, -resolution_));
+            nearby_.push_back(Eigen::Vector3d(resolution_, resolution_, 0.0));
+            nearby_.push_back(Eigen::Vector3d(resolution_, -resolution_, 0.0));
+            nearby_.push_back(Eigen::Vector3d(resolution_, 0.0, resolution_));
+            nearby_.push_back(Eigen::Vector3d(resolution_, 0.0, -resolution_));
+            nearby_.push_back(Eigen::Vector3d(0.0, resolution_, resolution_));
+            nearby_.push_back(Eigen::Vector3d(0.0, resolution_, -resolution_));
+            nearby_.push_back(Eigen::Vector3d(-resolution_, resolution_, 0.0));
+            nearby_.push_back(Eigen::Vector3d(-resolution_, -resolution_, 0.0));
+            nearby_.push_back(Eigen::Vector3d(-resolution_, 0.0, resolution_));
+            nearby_.push_back(Eigen::Vector3d(-resolution_, 0.0, -resolution_));
+            nearby_.push_back(Eigen::Vector3d(0.0, -resolution_, resolution_));
+            nearby_.push_back(Eigen::Vector3d(0.0, -resolution_, -resolution_));
+            nearby_.push_back(Eigen::Vector3d(resolution_, resolution_, resolution_));
+            nearby_.push_back(Eigen::Vector3d(resolution_, resolution_, -resolution_));
+            nearby_.push_back(Eigen::Vector3d(resolution_, -resolution_, resolution_));
+            nearby_.push_back(Eigen::Vector3d(resolution_, -resolution_, -resolution_));
+            nearby_.push_back(Eigen::Vector3d(-resolution_, resolution_, resolution_));
+            nearby_.push_back(Eigen::Vector3d(-resolution_, resolution_, -resolution_));
+            nearby_.push_back(Eigen::Vector3d(-resolution_, -resolution_, resolution_));
+            nearby_.push_back(Eigen::Vector3d(-resolution_, -resolution_, -resolution_));
+            break;
+    }
+}
+
+bool VoxelMap::isSameGrid(const Eigen::Vector3d& p1, const Eigen::Vector3d& p2) {
+    size_t p1_x = floor(p1.x() * resolution_inv);
+    size_t p1_y = floor(p1.y() * resolution_inv);
+    size_t p1_z = floor(p1.z() * resolution_inv);
+
+    size_t p2_x = floor(p2.x() * resolution_inv);
+    size_t p2_y = floor(p2.y() * resolution_inv);
+    size_t p2_z = floor(p2.z() * resolution_inv);
+    return p1_x == p2_x && p1_y == p2_y && p1_z == p2_z;
+}
+
+size_t VoxelMap::addCloud(const sensors::PointNormalCloud::Ptr cloud_ptr) {
+    size_t error_count = 0;
+    current_idx_.clear();
+    // 遍历所有点
+    for (sensors::PointNormalType& point : cloud_ptr->points) {
+        // 计算坐标值
+        Eigen::Vector3d pt_eigen(point.x, point.y, point.z);
+        size_t grid_loc = hash_util_(pt_eigen);
+        // 判断是否在voxel_map中
+        current_idx_.push_back(grid_loc);
+        auto iter = storage_.find(grid_loc);
+        // 不存在
+        if (iter == storage_.end()) {
+            std::shared_ptr<Grid> grid = std::make_shared<Grid>(grid_capacity_);
+            grid->addPoint(pt_eigen, true);
+            // 放入LRU缓存中，放在头部
+            cache_.push_front(grid_loc);
+            storage_.insert({grid_loc, {cache_.begin(), grid}});
+            if (storage_.size() >= capacity_) {
+                // 删除尾部的缓存
+                storage_.erase(cache_.back());
+                cache_.pop_back();
+            }
+        } else {
+            // 判断是否在同一个体素中，根据体素中的点云均值
+            if (!isSameGrid(iter->second.second->centroid, pt_eigen)) {
+                // 创建新的grid
+                error_count++;
+                cache_.erase(iter->second.first);
+                std::shared_ptr<Grid> grid = std::make_shared<Grid>(grid_capacity_);
+                grid->addPoint(pt_eigen, true);
+                cache_.push_front(grid_loc);
+                storage_[grid_loc].first = cache_.begin();
+                storage_[grid_loc].second = grid;
+            } else {
+                // 同一个体素，更新voxelmap
+                size_t num_in_grid = iter->second.second->point_num;
+                if (num_in_grid < 50) {
+                    bool insert = num_in_grid < grid_capacity_ ? true : false;
+                    iter->second.second->addPoint(pt_eigen, insert);
+                }
+                // 将cache_中元素iter->second.first 移动到cache_的头部，表示体素添加新的点
+                cache_.splice(cache_.begin(), cache_, iter->second.first);
+                storage_[grid_loc].first = cache_.begin();
+            }
+        }
+    }
+    // 遍历当前的体素，是否更新协防差
+    for (auto& idx : current_idx_) {
+        storage_[idx].second->updateConv();
+    }
+    return error_count;
+}
+
+void VoxelMap::reset() {
+    cache_.clear();
+    storage_.clear();
+}
+
+bool VoxelMap::searchKnn(const Eigen::Vector3d& point, size_t K, double range_threshold,
+                         std::vector<Eigen::Vector3d>& results) {
+    // 存放点和距离
+    std::vector<std::pair<Eigen::Vector3d, double>> candidates;
+    double range2 = range_threshold * range_threshold;
+    for (const Eigen::Vector3d& near : nearby_) {
+        Eigen::Vector3d near_by = point + near;
+        size_t hash_idx = hash_util_(near_by);
+        auto iter = storage_.find(hash_idx);
+        if (iter != storage_.end() && isSameGrid(near_by, iter->second.second->centroid)) {
+            // 点到周围的体素的距离小于range
+            for (const Eigen::Vector3d& pt : iter->second.second->points) {
+                double dist = (point - pt).squaredNorm();
+                if (dist < range_threshold) {
+                    // candidates.push_back({near_by, hash_idx});
+                    candidates.emplace_back(pt, dist);
+                }
+            }
+        }
+    }
+    if (candidates.empty()) {
+        return false;
+    }
+    // 利用std库找到第k小的数
+    if (candidates.size() > K) {
+        // 经过nth_element, K之前的元素都小于第K个元素，K之后的元素都大于第k个元素
+        std::nth_element(candidates.begin(), candidates.begin() + K - 1, candidates.end(),
+                         [](const auto& p1, const auto& p2) { return p1.second < p2.second; });
+        candidates.resize(K);
+    }
+    // 在排序一次
+    std::sort(candidates.begin(), candidates.end(),
+              [](const auto& p1, const auto& p2) { return p1.second < p2.second; });
+    results.clear();
+    for (const auto& it : candidates) {
+        results.emplace_back(it.first);
+    }
+    return true;
+}
+
+bool VoxelMap::getCentroidAndConvariance(size_t hash_idx, Eigen::Vector3d& centroid, Eigen::Matrix3d& conv) {
+    auto iter = storage_.find(hash_idx);
+    if (iter != storage_.end()) {
+        centroid = iter->second.second->centroid;
+        conv = iter->second.second->conv;
+        return true;
+    }
+    return false;
+}
+
+bool VoxelMap::getCentroidAndConvariance(const Eigen::Vector3d& point, Eigen::Vector3d& centroid,
+                                         Eigen::Matrix3d& conv) {
+    size_t hash_idx = hash_util_(point);
+    return getCentroidAndConvariance(hash_idx, centroid, conv);
+}
+
+void FastGrid::addPoint(const Eigen::Vector3d& point) {
+    num += 1;
+    centroid += ((point - centroid) / static_cast<double>(num));
+}
+
+FastVoxelMap::FastVoxelMap(double resolution) : resolution_(resolution), hash_util_(resolution) {
+    // 初始化周围的搜索范围
+    search_range_.clear();
+    for (int x_gain = -1; x_gain <= 1; ++x_gain) {
+        for (int y_gain = -1; y_gain <= 1; ++y_gain) {
+            for (int z_gain = -1; z_gain <= 1; ++z_gain) {
+                search_range_.emplace_back(
+                    // 周围q个点
+                    Eigen::Vector3d(x_gain * resolution, y_gain * resolution, z_gain * resolution));
+            }
+        }
+    }
+}
+// 对当前的点云进行滤波？
+void FastVoxelMap::filter(sensors::PointNormalCloud::Ptr cloud_ptr, std::vector<PointWithCov>& outs) {
+    if (cloud_ptr->empty()) {
+        return;
+    }
+    outs.clear();
+    grid_array_.clear();
+    grid_map_.clear();
+    grid_array_.reserve(cloud_ptr->size());
+    for (const sensors::PointNormalType& point : cloud_ptr->points) {
+        Eigen::Vector3d pt_eigen(point.x, point.y, point.z);
+        size_t idx = hash_util_(pt_eigen);
+        auto iter = grid_map_.find(idx);
+        if (iter == grid_map_.end()) {
+            std::shared_ptr<FastGrid> fast_grid = std::make_shared<FastGrid>();
+            fast_grid->addPoint(pt_eigen);
+            grid_map_.insert({idx, fast_grid});
+            grid_array_.push_back(fast_grid);
+        } else {
+            iter->second->addPoint(pt_eigen);
+        }
+    }
+    // 遍历所有的grid
+    for (const auto& grid : grid_array_) {
+        Eigen::Vector3d points_sum = Eigen::Vector3d::Zero();
+        Eigen::Matrix3d cov = Eigen::Matrix3d::Zero();
+        size_t points_num = 0;
+        // 遍历周围的几个体素
+        for (const Eigen::Vector3d& p : search_range_) {
+            Eigen::Vector3d near = grid->centroid + p;
+            auto hash_idx = hash_util_(near);
+            auto iter = grid_map_.find(hash_idx);
+            if (iter != grid_map_.end()) {
+                // 质心相加
+                points_sum += iter->second->centroid;
+                cov += iter->second->centroid * iter->second->centroid.transpose();
+                points_num++;
+            }
+        }
+        if (points_num >= 6) {
+            Eigen::Vector3d centroid = points_sum / static_cast<double>(points_num);
+            cov = (cov - points_sum * centroid.transpose()) / (static_cast<double>(points_num) - 1.0);
+            Eigen::JacobiSVD<Eigen::Matrix3d> svd(cov, Eigen::ComputeFullU | Eigen::ComputeFullV);
+            Eigen::Vector3d values(1, 1, 1e-3);
+            cov = svd.matrixU() * values.asDiagonal() * svd.matrixV().transpose();
+        }
+        outs.emplace_back(grid->centroid, cov);
+    }
+}
+}  // namespace lio
---
+++ src/voxel_map/voxel_map.hh
@@ -0,0 +1,121 @@
+#pragma once
+#include <Eigen/Core>
+#include <Eigen/Dense>
+#include <list>
+#include <memory>
+#include <unordered_map>
+#include <vector>
+#include "sensors/point_types.hh"
+
+namespace lio {
+struct PointWithCov {
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+    Eigen::Vector3d point;
+    Eigen::Matrix3d point_cov;
+    PointWithCov(const Eigen::Vector3d& p, const Eigen::Matrix3d& cov) : point(p), point_cov(cov) {
+    }
+};
+// 计算点的hash值
+class HashUtil {
+   public:
+    HashUtil() = default;
+    HashUtil(double resolution) : resolution_inv_(1.0 / resolution) {
+    }
+    HashUtil(double resolution, size_t hash_p, size_t max_n)
+        : resolution_inv_(1.0 / resolution), hash_p_(hash_p), max_n_(max_n) {
+    }
+    size_t operator()(const Eigen::Vector3d& point);
+
+   private:
+    size_t hash_p_ = 116101;
+    size_t max_n_ = 1e10;
+    double resolution_inv_ = 1.0;
+};
+
+struct Grid {
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+    Grid(size_t num) : max_num(num) {
+        points.reserve(2 * max_num);
+    }
+    size_t hash = 0;
+    size_t max_num = 20;
+    size_t min_num = 6;
+    size_t point_num = 0;
+    bool is_update = false;
+    bool is_valid = false;
+    std::vector<Eigen::Vector3d> points;
+    // 质心
+    Eigen::Vector3d centroid = Eigen::Vector3d::Zero();
+    Eigen::Matrix3d conv = Eigen::Matrix3d::Zero();
+    Eigen::Matrix3d conv_inv = Eigen::Matrix3d::Zero();
+    Eigen::Matrix3d conv_sum = Eigen::Matrix3d::Zero();
+    Eigen::Vector3d points_sum = Eigen::Matrix3d::Zero();
+    void setMinMax(size_t min_n, size_t max_n);
+    void updateConv();
+    void addPoint(const Eigen::Vector3d& point, bool insert);
+};
+
+class VoxelMap {
+   public:
+    enum NEARBY {
+        NEARBY1,
+        NEARBY7,
+        NEARBY19,
+        NEARBY26,
+    };
+    VoxelMap(double resolution, size_t capacity, size_t grid_capacity, NEARBY mode)
+        : resolution_(resolution),
+          resolution_inv(1.0 / resolution),
+          capacity_(capacity),
+          grid_capacity_(grid_capacity),
+          mode_(mode),
+          hash_util_(resolution) {
+        initializeNearby();
+    }
+
+    bool isSameGrid(const Eigen::Vector3d& p1, const Eigen::Vector3d& p2);
+    size_t addCloud(const sensors::PointNormalCloud::Ptr cloud_ptr);
+    void reset();
+    bool searchKnn(const Eigen::Vector3d& point, size_t K, double range_threshold,
+                   std::vector<Eigen::Vector3d>& results);
+    bool getCentroidAndConvariance(size_t hash_idx, Eigen::Vector3d& centroid, Eigen::Matrix3d& conv);
+    bool getCentroidAndConvariance(const Eigen::Vector3d& point, Eigen::Vector3d& centroid, Eigen::Matrix3d& conv);
+
+   private:
+    NEARBY mode_ = NEARBY::NEARBY7;
+    double resolution_ = 1.0;
+    double resolution_inv = 1.0;
+    size_t capacity_ = 5 * 1e6;
+    size_t grid_capacity_ = 20;
+    HashUtil hash_util_;
+    // 存储点的体素坐标，方便后续删除的操作
+    std::list<size_t> cache_;
+    // 存储周围的点
+    std::vector<Eigen::Vector3d> nearby_;
+    std::vector<size_t> current_idx_;
+    // 体素map
+    std::unordered_map<size_t, std::pair<std::list<size_t>::iterator, std::shared_ptr<Grid>>> storage_;
+
+   private:
+    void initializeNearby();
+};
+
+struct FastGrid {
+    Eigen::Vector3d centroid = Eigen::Vector3d::Zero();
+    size_t num = 0;
+    void addPoint(const Eigen::Vector3d& point);
+};
+
+class FastVoxelMap {
+   public:
+    FastVoxelMap(double resolution);
+    void filter(sensors::PointNormalCloud::Ptr cloud_ptr, std::vector<PointWithCov>& outs);
+
+   private:
+    HashUtil hash_util_;
+    double resolution_;
+    std::vector<Eigen::Vector3d> search_range_;
+    std::vector<std::shared_ptr<FastGrid>> grid_array_;
+    std::unordered_map<size_t, std::shared_ptr<FastGrid>> grid_map_;
+};
+}  // namespace lio
